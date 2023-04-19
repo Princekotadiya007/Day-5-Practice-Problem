62bca24ccf15ee285f375a1b52e88be699e421b7
Princekotadiya007 <princuuu77@gmail.com>
2023-04-19 16:41:05 +0530

    delete array

---
+++ Day-5-Prectice-Problem/Day-5-Prectice-Problem/DeleteArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Prectice_Problem
+{
+    public class DeleteArray
+    {
+        public static T[] DeleteMethod<T>(T[] arr, T elem)
+        {
+            int index = Array.IndexOf(arr, elem);
+
+            if (index != -1)
+            {
+                T[] result = new T[arr.Length - 1];
+                Array.Copy(arr, 0, result, 0, index);
+                Array.Copy(arr, index + 1, result, index, arr.Length - index - 1);
+                return result;
+            }
+            return arr;
+        }
+    }
+}
---
+++ Day-5-Prectice-Problem/Day-5-Prectice-Problem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Prectice_Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteArray delete = new DeleteArray();
+            int[] intArr = new int[] { 1, 2, 3, 4, 5 };
+            int[] newIntArr = DeleteArray.DeleteMethod(intArr, 3);
+            Console.WriteLine(string.Join(",", newIntArr));
+
+            double[] doubleArr = new double[] { 1.0, 2.5, 3.2, 4.7, 5.3 };
+            double[] newDoubleArr = DeleteArray.DeleteMethod(doubleArr, 4.7);
+            Console.WriteLine(string.Join(",", newDoubleArr));
+        }
+    }
+}
