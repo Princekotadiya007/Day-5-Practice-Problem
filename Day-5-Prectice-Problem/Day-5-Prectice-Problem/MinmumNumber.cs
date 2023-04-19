62692beef1261713f5035dc090b55e45dd04cc0f
Princekotadiya007 <princuuu77@gmail.com>
2023-04-19 16:55:43 +0530

    check minmum number

---
+++ Day-5-Prectice-Problem/Day-5-Prectice-Problem/MinmumNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Prectice_Problem
+{
+    internal class MinmumNumber
+    {
+        public void MinNumber()
+        {
+            Console.WriteLine("Enter three integers:");
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int min = num1; // assume num1 is the minimum
+
+            if (num2 < min)
+            {
+                min = num2;
+            }
+
+            if (num3 < min)
+            {
+                min = num3;
+            }
+
+            Console.WriteLine("Minimum value is: " + min);
+        }
+    }
+}
--- Day-5-Prectice-Problem/Day-5-Prectice-Problem/Program.cs
+++ Day-5-Prectice-Problem/Day-5-Prectice-Problem/Program.cs
@@ -10,14 +10,17 @@ namespace Day_5_Prectice_Problem
     {
         static void Main(string[] args)
         {
-            DeleteArray delete = new DeleteArray();
-            int[] intArr = new int[] { 1, 2, 3, 4, 5 };
-            int[] newIntArr = DeleteArray.DeleteMethod(intArr, 3);
-            Console.WriteLine(string.Join(",", newIntArr));
+            //DeleteArray delete = new DeleteArray();
+            //int[] intArr = new int[] { 1, 2, 3, 4, 5 };
+            //int[] newIntArr = DeleteArray.DeleteMethod(intArr, 3);
+            //Console.WriteLine(string.Join(",", newIntArr));
 
-            double[] doubleArr = new double[] { 1.0, 2.5, 3.2, 4.7, 5.3 };
-            double[] newDoubleArr = DeleteArray.DeleteMethod(doubleArr, 4.7);
-            Console.WriteLine(string.Join(",", newDoubleArr));
+            //double[] doubleArr = new double[] { 1.0, 2.5, 3.2, 4.7, 5.3 };
+            //double[] newDoubleArr = DeleteArray.DeleteMethod(doubleArr, 4.7);
+            //Console.WriteLine(string.Join(",", newDoubleArr));
+
+            MinmumNumber minmum = new MinmumNumber();
+            minmum.MinNumber();
         }
     }
 }
